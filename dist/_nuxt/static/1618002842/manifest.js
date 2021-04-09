ceabda58df846180e95debbc644f14fa1df45555
Kevin Zepeda <kevin@floups.com>
2021-04-09 16:14:03 -0500

    add dist folder to deploy

---
+++ dist/_nuxt/static/1618002842/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fnot-found","\u002F","\u002Fabout","\u002Fservices","\u002Fportfolio"]})
---
+++ dist/_nuxt/static/1618002842/services/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618002842",layout:"default",error:a,serverRendered:true,routePath:"\u002Fservices",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c99e9b2d"}}}}(null));
